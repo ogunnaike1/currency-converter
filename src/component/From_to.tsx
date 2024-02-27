8277763bdd02ef932fc23109bc9074a39b89dd87
ogunnaike1 <ogunnaikeusman17@gmail.com>
2024-02-27 17:43:44 +0100

    commit

--- src/component/From_to.tsx
+++ src/component/From_to.tsx
@@ -2,42 +2,56 @@ import React from "react";
 import { GoArrowSwitch } from "react-icons/go";
 
 const From_to = () => {
+  let currencies = [
+    { name: "USD", flag: "nigga" },
+    { name: "EUR", flag: "nigga" },
+    { name: "JPY", flag: "nigga" },
+    { name: "GBP", flag: "nigga" },
+    { name: "AUD", flag: "nigga" },
+    { name: "CAD", flag: "nigga" },
+    { name: "CHF", flag: "nigga" },
+    { name: "CNH", flag: "nigga" },
+    { name: "HKD", flag: "nigga" },
+    { name: "NZD", flag: "nigga" },
+    { name: "NGN", flag: "nigga" },
+  ];
+  currencies.sort((a, b) => a.name.localeCompare(b.name));
+  currencies.forEach(currency => {
+    console.log(currency.name);
+  });
+
   return (
     <>
-    
-    <div className="div-from_to">
+      <div className="div-from_to">
         <div className="div-from">
-            <span>From</span>
-      <select className="select-from">
-        <option>C</option>
-        <option>B</option>
-        <option>A</option>
-        <option>D</option>
-      </select>
+          <span>From</span>
+          <select className="select-from">
+            {currencies.map((currency) => (
+              <option key={currency.name}>{currency.name}</option>
+            ))}
+          </select>
+        </div>
+
+        <button>
+          <GoArrowSwitch />
+        </button>
+
+        <div className="div-to">
+          <span>To</span>
+          <select className="select-from">
+            {currencies.map((currency) => (
+              <option key={currency.name}>{currency.name}</option>
+            ))}
+          </select>
+        </div>
       </div>
 
-      <button><GoArrowSwitch /></button>
-
-      <div className="div-to">
-      <span>To</span>
-
-      <select id="select-to">
-        <option>C</option>
-        <option>B</option>
-        <option>A</option>
-        <option>D</option>
-      </select>
-      </div>
-    </div>
-
-    <div className="div-converter">
+      <div className="div-converter">
         <span>100 USD</span>
         <span>=</span>
         <span>1000 NGN</span>
-
-    </div>
+      </div>
     </>
-   
   );
 };
 
