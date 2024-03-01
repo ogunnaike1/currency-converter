899c5e5d0e83b75523b5f1eaf6095ff06bec3d27
ogunnaike1 <ogunnaikeusman17@gmail.com>
2024-03-01 15:40:33 +0100

    commit

--- src/component/Exchange_button.tsx
+++ src/component/Exchange_button.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import PropTypes from "prop-types";
 
-const Exchange_button = () => {
+const Exchange_button = (props: any) => {
   return (
-    <button className='exchange-button'>Get Exchange Rate</button>
+    <button className='exchange-button' onClick={props.onClick}>Get Exchange Rate</button>
   )
 }
+Exchange_button.propTypes = {
+    onClick: PropTypes.func
+}
+
 
 export default Exchange_button
--- src/component/From_to.tsx
+++ src/component/From_to.tsx
@@ -1,85 +1,130 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoArrowSwitch } from "react-icons/go";
-import ReactCountryflagTitle, { ReactCountryFlag } from "react-country-flag"
+import ReactCountryflagTitle, { ReactCountryFlag } from "react-country-flag";
+import Exchange_button from "./Exchange_button";
 
 const From_to = () => {
+  const [displayFlag, setDisplayFlag] = useState("");
+  const [displaySecondFlag, SetdisplaySecondFlag] = useState("");
+  const [amountChange, setAmountChange] = useState<number>();
+  const [initialValue, setInitialValue] = useState<number>();
+
   let currencies = [
-    { name: "USD", flagTitle: "US"  , title :"US"},
-    { name: "EUR", flagTitle: "nigga" , title :"US" },
-    { name: "JPY", flagTitle: "nigga" , title :"US"},
-    { name: "GBP", flagTitle: "nigga" , title :"US"},
-    { name: "AUD", flagTitle: "nigga" , title :"US"},
-    { name: "CAD", flagTitle: "nigga" , title :"US"},
-    { name: "CHF", flagTitle: "nigga" , title :"US"},
-    { name: "CNH", flagTitle: "nigga" , title :"US"},
-    { name: "HKD", flagTitle: "nigga" , title :"US"},
-    { name: "NZD", flagTitle: "nigga" , title :"US"},
-    { name: "NGN", flagTitle: "nigga" , title :"US"},
+    { countryName: "USD", flagTitle: "US", title: "US", toCountryName: "USD" },
+    { countryName: "EUR", flagTitle: "FR", title: "US", toCountryName: "EUR" },
+    { countryName: "JPY", flagTitle: "JP", title: "US", toCountryName: "JPY" },
+    { countryName: "GBP", flagTitle: "GB", title: "US", toCountryName: "GBP" },
+    { countryName: "AUD", flagTitle: "AU", title: "US", toCountryName: "AUD" },
+    { countryName: "CAD", flagTitle: "CA", title: "US", toCountryName: "CAD" },
+    { countryName: "CHF", flagTitle: "CH", title: "US", toCountryName: "CHF" },
+    { countryName: "CNH", flagTitle: "CN", title: "US", toCountryName: "CNH" },
+    { countryName: "HKD", flagTitle: "HK", title: "US", toCountryName: "HKD" },
+    { countryName: "NZD", flagTitle: "NZ", title: "US", toCountryName: "NZD" },
+    { countryName: "NGN", flagTitle: "NG", title: "US", toCountryName: "NGN" },
   ];
-  currencies.sort((a, b) => a.name.localeCompare(b.name));
-  currencies.forEach(currency => {
-    console.log(currency.name);
-  });
+  currencies.sort((a, b) => a.countryName.localeCompare(b.countryName));
+  currencies.sort((a, b) => a.toCountryName.localeCompare(b.toCountryName));
+
+  useEffect(() => {
+    setDisplayFlag(currencies[0].flagTitle);
+    SetdisplaySecondFlag(currencies[0].flagTitle);
+  }, []);
+
+  const handleChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setDisplayFlag(e.target.value);
+  };
+  const handleSecondChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    SetdisplaySecondFlag(e.target.value);
+  };
+
+  const handleAmountChange = (e: { target: { value: string; }; }) => {
+    const newValue: number = parseFloat(e.target.value);
+    setAmountChange(newValue);
+    setInitialValue(newValue);
+   
+  };
+  
+
+
+  const [exchangeRate, setExchangeRate] = useState(0)
+
+  const handleExchangeRate = () => {
+
+  }
 
   return (
     <>
+      <div className="div-enterAmount">Enter Amount</div>
+      <input type="number" onChange = {handleAmountChange}/>
       <div className="div-from_to">
         <div className="div-from">
           <span>From</span>
           <div className="div-image-container">
-          <ReactCountryFlag
-                countryCode="US"
-                svg
-                style={{
-                    width: '2em',
-                    height: '2em',
-                }}
-                title="US"
+            <ReactCountryFlag
+              countryCode={displayFlag}
+              svg
+              style={{
+                width: "2em",
+                height: "100%",
+              }}
+              title="US"
             />
-          <select className="select-from">
-            {currencies.map((currency) => (
-              <option key={currency.name} > {currency.name}</option>
-            ))}
-          </select>
+            <select
+              className="select-from"
+              value={displayFlag}
+              onChange={handleChange}
+            >
+              {currencies.map((currency) => (
+                <option key={currency.countryName} value={currency.flagTitle}>
+                  {" "}
+                  {currency.countryName}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
-        <div>
-
-        </div>
+        <div></div>
         <button>
           <GoArrowSwitch />
         </button>
-
         <div className="div-to">
           <span>To</span>
           <div className="div-image-container">
-          <ReactCountryFlag
-                countryCode="US"
-                svg
-                style={{
-                    width: '2em',
-                    height: '2em',
-                }}
-                title="US"
+            <ReactCountryFlag
+              countryCode={displaySecondFlag}
+              svg
+              style={{
+                width: "2em",
+                height: "2em",
+              }}
+              title="US"
             />
-          <select className="select-from">
-            {currencies.map((currency) => (
-              <option key={currency.name} > {currency.name}</option>
-            ))}
-          </select>
+            <select
+              className="select-from"
+              value={displaySecondFlag}
+              onChange={handleSecondChange}
+            >
+              {currencies.map((currency) => (
+                <option key={currency.toCountryName} value={currency.flagTitle}>
+                  {" "}
+                  {currency.toCountryName}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
-      
-
-      <div>
-        
-      </div>
+      <div></div>
       <div className="div-converter">
-        <span>100 USD</span>
+        <span>{initialValue} USD</span>
         <span>=</span>
         <span>1000 NGN</span>
       </div>
+      <Exchange_button onClick = {handleExchangeRate} />
     </>
   );
 };
